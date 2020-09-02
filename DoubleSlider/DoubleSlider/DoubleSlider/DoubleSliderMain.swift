b2ec2291453dcf8b3e1f54acef394b21ffc08add
git-master101 <70585569+git-master101@users.noreply.github.com>
2020-09-02 10:05:26 +0200

    Update DoubleSliderMain.swift

--- DoubleSlider/DoubleSlider/DoubleSlider/DoubleSliderMain.swift
+++ DoubleSlider/DoubleSlider/DoubleSlider/DoubleSliderMain.swift
@@ -3,7 +3,7 @@
 //  DoubleSlider
 //
 //  Created by josh on 2018/03/30.
-//  Copyright © 2018年 yhkaplan. All rights reserved.
+//  Copyright © 2018年 yhkaplan. All rights reserved.ios
 //
 
 import UIKit
